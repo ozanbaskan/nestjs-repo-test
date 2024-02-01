3e297dd26afb7118bd1501e9ae87faacdea1216b
ozanbaskan <ozanbaskan7@gmail.com>
2024-02-01 09:06:04 +0400

    format

--- src/declarations/array.d.ts
+++ src/declarations/array.d.ts
@@ -1,7 +1,10 @@
-import { ModelData } from "./sequelize";
+import { ModelData } from './sequelize';
 
 declare global {
-    interface Array<T> {
-      eachToJSON(): ModelData<T>[];
-    }
+  interface Array<T> {
+    /**
+     * Custom global declaration for Array.prototype to use for arrays that sequelize ORM returns
+     */
+    eachToJSON(): ModelData<T>[];
+  }
 }
