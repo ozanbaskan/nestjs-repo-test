fbc39b2cc081b62c0a4db1de1c8177902835c822
ozanbaskan <ozanbaskan7@gmail.com>
2024-02-01 09:29:47 +0400

    fix type

--- src/declarations/array.d.ts
+++ src/declarations/array.d.ts
@@ -1,10 +1,11 @@
 import { ModelData } from './sequelize';
+import { Model } from 'sequelize-typescript';
 
 declare global {
   interface Array<T> {
     /**
      * Custom global declaration for Array.prototype to use for arrays that sequelize ORM returns
      */
-    eachToJSON(): ModelData<T>[];
+    eachToJSON(): T extends Model ? ModelData<T>[] : any[];
   }
 }
